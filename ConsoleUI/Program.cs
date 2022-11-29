39dc6cf22291f1b7e6e1a8fb526003f7d9c756d2
Fevzi Böyükparmaksız <fevzibparmaksiz.gmail.comgit config --global user.email fevzibparmaksiz.gmail.comgit config --global user.name Fevzi>
2022-11-30 00:38:09 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetByUnitPrice(40,100))
+{
+    Console.WriteLine(product.ProductName); 
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // Generic Constraint
+    // Class : Referans tip 
+    // IEntity olabilir ya da onu implemente eden bir nesne olabilir
+    // new(): newlenebilir olmalı
+    public interface IEntityRepository<T> where T : class, IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);  // =null filtre vermeyedebilirsin demek !!!    
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
