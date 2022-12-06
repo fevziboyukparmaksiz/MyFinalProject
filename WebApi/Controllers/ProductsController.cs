56f26794ad48f6cc8f554f827e8d27f4542f44db
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2022-12-06 22:48:16 +0300

    Web Api

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely Coupled
+        //Naming convention
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService=productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Getall()
+        {
+            //Dependency Chain
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
